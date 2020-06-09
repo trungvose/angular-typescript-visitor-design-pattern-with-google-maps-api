581396f8951870514561dd8b2252fc68da8af551
trungk18 <trungk18@gmail.com>
2020-06-09 10:54:21 +0800

    1 file modified

--- src/app/model/marker/map-icon.ts
+++ src/app/model/marker/map-icon.ts
@@ -1,5 +1,5 @@
 export class MapIcon {
-    static BASE_URL = "https://raw.githubusercontent.com/trungk18/trungk18.github.io/master/img/map/";
+    static BASE_URL = "https://gitlab.com/trungk181/blog/-/raw/master/img/map/";
     static POINT_ICON = `${MapIcon.BASE_URL}/point-marker-green.png`;
     static START_LOCATION_ICON = `${MapIcon.BASE_URL}/beachflag.png	`;
     static REAL_TIME_ICON = `${MapIcon.BASE_URL}/bluedot.png`;
